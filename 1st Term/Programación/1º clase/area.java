5bc58f1fb39fc20af7119f5b7653ae28d653b8fa
Borrajo <119629992+b0rrajo@users.noreply.github.com>
2025-09-16 18:51:11 +0200

    Area - use of doubles

---
+++ 1st Term/Programación/1º clase/area.java
@@ -0,0 +1,8 @@
+public class area{
+    public static void main(String[]args){
+        double base = 10;
+        double altura = 20;
+        double area = (base * altura);
+        System.output.println("La base del elemento calculado es: " + area);
+    }
+}
